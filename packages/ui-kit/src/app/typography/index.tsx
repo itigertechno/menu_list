a727fa0871fcaba062eb3c0547e58077630da868
Sequencer <45060278+sequencerr@users.noreply.github.com>
2024-12-13 12:03:08 +0100

    Optimize typography components

--- packages/ui-kit/src/app/typography/index.tsx
+++ packages/ui-kit/src/app/typography/index.tsx
@@ -1,31 +1,18 @@
 import s from './typography.module.scss';
 
-type TextElem = Readonly<{ children: React.JSX.Element }>;
+type Typography = (props: Readonly<{ children: React.JSX.Element }>) => React.JSX.Element;
 
-export const Heading1 = ({ children }: TextElem) => <span className={s.heading1}>{children}</span>;
-
-export const Heading2 = ({ children }: TextElem) => <span className={s.heading2}>{children}</span>;
-
-export const Heading3 = ({ children }: TextElem) => <span className={s.heading3}>{children}</span>;
-
-export const Heading4 = ({ children }: TextElem) => <span className={s.heading4}>{children}</span>;
-
-export const Heading5 = ({ children }: TextElem) => <span className={s.heading5}>{children}</span>;
-
-export const TextMain = ({ children }: TextElem) => <span className={s.textMain}>{children}</span>;
-
-export const RegularNavigation = ({ children }: TextElem) => <span className={s.regularNavigation}>{children}</span>;
-
-export const ActiveNavigation = ({ children }: TextElem) => <span className={s.activeNavigation}>{children}</span>;
-
-export const TextButton = ({ children }: TextElem) => <span className={s.textButton}>{children}</span>;
-
-export const TextOfElements = ({ children }: TextElem) => <span className={s.textOfElements}>{children}</span>;
-
-export const CardContent = ({ children }: TextElem) => <span className={s.cardContent}>{children}</span>;
-
-export const Subtitle = ({ children }: TextElem) => <span className={s.subtitle}>{children}</span>;
-
-export const CardTitle = ({ children }: TextElem) => <span className={s.cardTitle}>{children}</span>;
-
-export const Small = ({ children }: TextElem) => <span className={s.small}>{children}</span>;
+export const Heading1: Typography = p => <span className={s.heading1}>{p.children}</span>;
+export const Heading2: Typography = p => <span className={s.heading2}>{p.children}</span>;
+export const Heading3: Typography = p => <span className={s.heading3}>{p.children}</span>;
+export const Heading4: Typography = p => <span className={s.heading4}>{p.children}</span>;
+export const Heading5: Typography = p => <span className={s.heading5}>{p.children}</span>;
+export const TextMain: Typography = p => <span className={s.textMain}>{p.children}</span>;
+export const RegularNavigation: Typography = p => <span className={s.regularNavigation}>{p.children}</span>;
+export const ActiveNavigation: Typography = p => <span className={s.activeNavigation}>{p.children}</span>;
+export const TextButton: Typography = p => <span className={s.textButton}>{p.children}</span>;
+export const TextOfElements: Typography = p => <span className={s.textOfElements}>{p.children}</span>;
+export const CardContent: Typography = p => <span className={s.cardContent}>{p.children}</span>;
+export const Subtitle: Typography = p => <span className={s.subtitle}>{p.children}</span>;
+export const CardTitle: Typography = p => <span className={s.cardTitle}>{p.children}</span>;
+export const Small: Typography = p => <span className={s.small}>{p.children}</span>;
