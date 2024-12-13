138cf73133782b2496b7e3b4c304e8b35e60317e
Sequencer <45060278+sequencerr@users.noreply.github.com>
2024-12-13 15:52:43 +0100

    Disaplay all demo pages at home altogether merged

--- packages/ui-kit/src/app/(home)/page.tsx
+++ packages/ui-kit/src/app/(home)/page.tsx
@@ -1,6 +1,27 @@
-import Image from 'next/image';
+'use client';
+
 import styles from './page.module.css';
+import { Button } from '@/app/buttons';
+import Cards from '@/app/card/page';
+import Fonts from '@/app/fonts/page';
+import Icons from '@/app/icons/page';
+import { Footer } from '@/app/footer';
+import Typograpgy from '@/app/typography/page';
 
 export default function Home() {
-	return <div className={styles.page}></div>;
+	return (
+		<div className={styles.page}>
+			<Button onClick={() => {}}>Demo Button</Button>
+
+			<Cards />
+
+			<Fonts />
+
+			<Footer />
+
+			<Icons />
+
+			<Typograpgy />
+		</div>
+	);
 }
